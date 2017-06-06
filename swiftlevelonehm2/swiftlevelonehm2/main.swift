3b5eabb8fb4985a6006d2aa3965130859e19082d
Александр Ломтев <aleksandrlomtev@MacBook-Pro-Aleksandr.local>
2017-06-06 19:55:15 +0300

    1

---
+++ swiftlevelonehm2/swiftlevelonehm2/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  swiftlevelonehm2
+//
+//  Created by Александр Ломтев on 06.06.17.
+//  Copyright © 2017 Александр Ломтев. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
